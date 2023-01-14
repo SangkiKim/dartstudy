195dec9aa19f1e851b408bcb56eab6073aedba19
Sangki Kim <goodki283@gmail.com>
2023-01-14 15:10:18 +0900

    named constructor

--- main.dart
+++ main.dart
@@ -1,11 +1,44 @@
-String sayHello({required String name, required int age, required String country}) => "Hello $name $age from $country nice to meet you";
+class Player {
+  final String name;
+  int xp,age;
+  String team;
+ 
+  Player({required this.name, 
+          required this.xp, 
+          required this.team, 
+          required this.age,
+         });
+  
+  Player.createBluePlayer({
+    required String name, 
+    required int age
+  }) :  this.age = age,
+        this.name = name,
+        this.team = 'blue',
+        this.xp = 0;
+  
+  Player.createRedPlayer(
+    String name, 
+    int age
+  ) :  this.age = age,
+        this.name = name,
+        this.team = 'red',
+        this.xp = 0;
+  
+  void sayHello(){
+    print("Hello my name is $name and my xp is $xp and my age is $age my team is $team");
+  }
+}
 
-num plus(num a , num b) => a + b;
-
-void main() {
-  sayHello(
-    name: 'ken',
-    age:12,
-    country: 'canada'
+void main(){
+  var bluePlayer = Player.createBluePlayer(
+    name: "sangki",
+    age: 29,
   );
+  var redPlayer = Player.createRedPlayer(
+    "nico",
+    22,
+  );
+  bluePlayer.sayHello();
+  redPlayer.sayHello();
 }
